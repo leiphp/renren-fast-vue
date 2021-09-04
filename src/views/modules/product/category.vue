9c753098169bdedd51cbb413a30a6881605416e7
leiwen <657106593@qq.com>
2021-09-04 23:13:12 +0800

    feat:完成菜单删除功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -5,6 +5,7 @@
     :expand-on-click-node="false"
     show-checkbox
     node-key="catId"
+    :default-expanded-keys="expandedKey"
   >
     <span class="custom-tree-node" slot-scope="{ node, data }">
       <span>{{ node.label }}</span>
@@ -37,6 +38,7 @@ export default {
   data() {
     return {
       menus: [],
+      expandedKey: [],
       defaultProps: {
         children: "children",
         label: "name",
@@ -59,7 +61,30 @@ export default {
       console.log("append", data);
     },
     remove(node, data) {
-      console.log("append", data);
+      var ids = [data.catId];
+      this.$confirm(`是否删除【${data.name}】菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl("/product/category/delete"),
+          method: "post",
+          data: this.$http.adornData(ids, false),
+        }).then(({ data }) => {
+          this.$message({
+            message: "菜单删除成功",
+            type: "success"
+          });
+          //刷新菜单
+          this.getMenus();
+          //设置默认展开的菜单
+          this.expandedKey = [node.parent.data.catId]
+        });
+      }).catch(()=>{
+
+      });
+      console.log("remove", node, data);
     },
   },
   created() {
