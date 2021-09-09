552f66fb1d87146a9694c0331e81e00988818ace
leiwen <657106593@qq.com>
2021-09-10 01:57:01 +0800

    feat:修改排序0的bug

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -79,7 +79,7 @@ import SingleUpload from "@/components/upload/singleUpload"
           ],
           sort: [
             { validator: (rule,value,callback)=>{
-              if(value==''){
+              if(value===''){
                 callback(new Error('排序字段必须填写'));
               }else if(!Number.isInteger(value) || value<0){
                 callback(new Error('排序必须是一个大于等于0的整数'));
